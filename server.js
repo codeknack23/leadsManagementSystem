78d1d98027fd6bec2335930a9edfba7efb0a0547
ronak <ronakbhojayya@gmail.com>
2025-09-27 15:26:50 +0530

    api added

--- server.js
+++ server.js
@@ -132,12 +132,6 @@ app.delete("/api/leads/:id", authMiddleware, async (req, res) => {
 
 
 
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
